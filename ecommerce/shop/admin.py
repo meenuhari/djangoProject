1d5a092a47f969b5d4c868bf922e7e6e02efd0f6
unknown <meenuharikumar2001@gmail.com>
2024-02-05 20:41:30 +0530

    added

---
+++ ecommerce/shop/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from shop.models import Category
+
+from shop.models import Product
+
+# Register your models here.
+admin.site.register(Category)
+admin.site.register(Product)
